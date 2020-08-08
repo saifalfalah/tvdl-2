d4cdc8e1f14f9924b1c4cb6aa97e7893ad835b01
Saif Al Falah <saifalfalah@gmail.com>
2020-08-08 16:42:03 +0530

    step 2 complete

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -1,15 +1,26 @@
 let arc = require("@architect/functions");
 let parseBody = arc.http.helpers.bodyParser;
 let isURL = require("is-url");
+const parse = require("url-parse");
 
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 
 let checkBodyUrl = (body) => {
-  if (!body.url) throw new Error("601: No url");
+  if (!body.url) throw new Error("600: No url found");
 };
 
-let checkIsUrl = (body) => {
-  if (!isURL(body.url)) throw new Error("602: Not a url");
+let checkIsUrl = (url) => {
+  if (!isURL(url)) throw new Error("601: Not a url");
+};
+
+let checkIfTwitterUrl = (url) => {
+  const parsedUrl = parse(url);
+  if (
+    parsedUrl.hostname !== "twitter.com" &&
+    parsedUrl.hostname !== "www.twitter.com" &&
+    parsedUrl.hostname !== "mobile.twitter.com"
+  )
+    throw new Error("602: Not a twitter url");
 };
 
 exports.handler = async function http(req) {
@@ -21,7 +32,13 @@ exports.handler = async function http(req) {
     // 1. Check if the body contains the field url
     checkBodyUrl(body);
     // 2. Check if the field url in the body is a url
-    checkIsUrl(body);
+    const url = body.url;
+    checkIsUrl(url);
+    // 2. Check if twitter URL
+    checkIfTwitterUrl(url);
+    // 3. Check if the URL contains
+
+    // return something only if there are no errors.
     return {
       headers: {
         "cache-control":
@@ -41,8 +58,6 @@ exports.handler = async function http(req) {
       body: `<div>${e.message}</div>`,
     };
   } finally {
-    // 2. Check if twitter URL
-    // 3. Check if the URL contains
     // console.log("finally");
     // Save data in begin here
   }
