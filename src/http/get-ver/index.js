ac90c77b2252b28faedb25895070373efe74a09e
Saif Al Falah <saifalfalah@gmail.com>
2020-08-10 01:43:52 +0530

    adding link to the shortcut too

--- src/http/get-ver/index.js
+++ src/http/get-ver/index.js
@@ -1,6 +1,9 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 exports.handler = async function http(req) {
-  const latest = 5000;
+  const latest = {
+    ver: 5000,
+    link: "https://www.icloud.com/shortcuts/41ed3432ea1e4bc48e881ad608bb355f",
+  };
   return {
     headers: {
       "content-type": "application/json; charset=utf8",
