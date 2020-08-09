349a90d1f55fed57005dab17251b1dcada913252
Saif Al Falah <saifalfalah@gmail.com>
2020-08-09 18:01:15 +0530

    returning the json for a successful request

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -70,11 +70,9 @@ exports.handler = async function http(req) {
     // return something only if there are no errors.
     return {
       headers: {
-        "cache-control":
-          "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
-        "content-type": "text/html; charset=utf8",
+        "content-type": "application/json; charset=utf8",
       },
-      body: `<div>Hello World</div>`,
+      body: JSON.stringify({ downloadObject }),
     };
   } catch (e) {
     console.log(e.message);
