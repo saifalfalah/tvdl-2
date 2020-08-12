af25e430f77c35d061b44f6f1941a4a7a26e81c9
Saif Al Falah <saifalfalah@gmail.com>
2020-08-13 02:54:06 +0530

    testing

--- src/http/post-try/index.js
+++ src/http/post-try/index.js
@@ -15,19 +15,19 @@ exports.handler = async function http(req) {
       high: {
         downloadURL:
           "https://video.twimg.com/ext_tw_video/1050225629256278016/pu/vid/720x1280/V-g_RcEfrouW4HjN.mp4?tag=5",
-        size: "1.72 MB",
+        size: "NAN",
       },
       medium: {
         downloadURL:
           "https://video.twimg.com/ext_tw_video/1050225629256278016/pu/vid/360x640/BGsKVAaCQXB2ds7f.mp4?tag=5",
-        size: "0.66 MB",
+        size: "NAN",
       },
       low: {
         downloadURL:
           "https://video.twimg.com/ext_tw_video/1050225629256278016/pu/vid/180x320/1TDydRL_AFvzgf4s.mp4?tag=5",
         size: "0.2 MB",
       },
-      sell: true,
+      sell: false,
       sellLink: "https://buymeacoffee.com/saif",
       sellMessage:
         "Thank you for using TVDL. Supporting millions of users costs quite a lot of money. If you like this shortcut, please consider helping me by supporting this shortcut.",
