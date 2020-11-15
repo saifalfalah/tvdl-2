c510e3987bf3773e38e5a04c1e34b9e49dbc62c9
Saif Al Falah <saifalfalah@gmail.com>
2020-11-16 02:35:41 +0530

    testing update on ios 12

--- src/http/post-index/helpers.js
+++ src/http/post-index/helpers.js
@@ -224,7 +224,7 @@ exports.appendLatestVersionInformation = (downloadObject, ver) => {
     // for iOS 12
     switch (ver) {
       case 1307:
-        downloadObject["ver"] = 1308;
+        downloadObject["ver"] = 1307;
         // TODO: Update this link
         downloadObject["latestLink"] =
           "https://www.icloud.com/shortcuts/a72f16f4e3664c8daa0c0a5fc76182fe";
