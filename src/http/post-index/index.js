4782ffd729975b9187e84b70b812eebbe424ce51
Saif Al Falah <saifalfalah@gmail.com>
2021-11-24 22:31:45 +0530

    logging error.data

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -29,7 +29,7 @@ axios.interceptors.response.use(
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    console.log(error);
+    console.log(error.data);
     return Promise.reject(error);
   }
 );
