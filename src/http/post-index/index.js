812513dbe257a3cc8f2c2391da3434f2370ca107
Saif Al Falah <saifalfalah@gmail.com>
2021-11-24 22:57:17 +0530

    removed axios interceptor

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -19,22 +19,6 @@ const {
   logError,
 } = require("./helpers");
 
-// Add a response interceptor
-axios.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    // console.log(error);
-    console.log(error.response);
-    return Promise.reject(error);
-  }
-);
-
 exports.handler = async function http(req) {
   let body = parseBody(req);
   let didUpsell = false;
