488f0df0ee02ef75d5ac63ced88ae2e62f19fe7f
Saif Al Falah <saifalfalah@gmail.com>
2021-12-18 23:12:18 +0530

    removed comments. added package

--- src/http/post-groot/index.js
+++ src/http/post-groot/index.js
@@ -7,13 +7,7 @@ exports.handler = async function http(req) {
   let body = parseBody(req);
   try {
     let url = body.url;
-
-    // 4. Check if the URL contains a video
-    // Getting the Tweet Path
-
     const tweetPath = parse(url).pathname.split("/")[3];
-
-    // 5. Prepare api request url
     const requestUrl = `https://api.twitter.com/1.1/statuses/show.json?id=${tweetPath}&tweet_mode=extended`;
 
     let data;
@@ -37,7 +31,6 @@ exports.handler = async function http(req) {
   } catch (e) {
     console.error("error message", e.message);
 
-    // returning the error
     return {
       headers: {
         "content-type": "application/json; charset=utf8",
@@ -45,11 +38,5 @@ exports.handler = async function http(req) {
       body: JSON.stringify(e.message),
       statusCode: 400,
     };
-  } finally {
-    // Save data in db here
-    // table = requests
-    // key = date
-    // prop = total number of requests today
-    // value = number of requests today
   }
 };
