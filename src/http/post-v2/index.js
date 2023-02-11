7568e3746d3c43be98803bec0846a88b29b84fd5
Saif Al Falah <saifalfalah@gmail.com>
2023-02-11 15:57:12 +0530

    support for images and videos

--- src/http/post-v2/index.js
+++ src/http/post-v2/index.js
@@ -1,5 +1,21 @@
+const Sdk = require("twitter-api-sdk");
+// import { Client } from "twitter-api-sdk";
+
+const client = new Sdk.Client(process.env.TOKEN2);
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 exports.handler = async function http(req) {
+  const tweet = await client.tweets.findTweetById("1577282815250427907", {
+    expansions: ["attachments.media_keys"],
+    "media.fields": [
+      "alt_text",
+      "duration_ms",
+      "preview_image_url",
+      "public_metrics",
+      "variants",
+    ],
+  });
+  console.log(tweet);
+  console.log(JSON.stringify(tweet.includes.media[0]));
   const latest = {
     ver: 5000,
     link: "https://www.tvdl.app",
@@ -8,6 +24,6 @@ exports.handler = async function http(req) {
     headers: {
       "content-type": "application/json; charset=utf8",
     },
-    body: JSON.stringify(latest),
+    body: tweet.includes.media,
   };
 };
